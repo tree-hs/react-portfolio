a9ce98d2d7e5489b7e3949d650ce2296d349dee9
han-seok,Jeong <harrison14@naver.com>
2025-12-08 21:41:10 +0900

    first-commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 My Site</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>사이트 로고</h1>
+      {/* 메뉴 등 */}
+    </header>
+  );
+}
+
+export default Header;
